717ac678b74a0691bd062061498cc001e411d482
Samuel-Hite <144499940+Samuel-Hite@users.noreply.github.com>
2023-11-21 14:59:15 -0500

    Add files via upload

---
+++ audioApp/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  testAudioApp
+//
+//  Created by Sam Hite on 11/7/23.
+//
+
+import SwiftUI
+import AVFoundation
+
+
+struct ContentView : View {
+    var body: some View {
+        AudioRecordView()
+    }
+}
+
